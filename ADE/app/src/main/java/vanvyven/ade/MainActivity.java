60366e20c46d7273a7f90f18c71cd7a597869d07
Nicolas Vanvyve <nicolas.vanvyve@student.uclouvain.be>
2018-02-12 23:48:25 +0100

    firsts buttons

--- ADE/app/src/main/java/vanvyven/ade/MainActivity.java
+++ ADE/app/src/main/java/vanvyven/ade/MainActivity.java
@@ -1,7 +1,10 @@
 package vanvyven.ade;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ade = (Button) findViewById(R.id.ade_button);
+        ade.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent s = new Intent(MainActivity.this,ADEActivity.class);
+                startActivity(s);
+            }
+        });
+
+        Button settings = (Button) findViewById(R.id.settings_button);
+        settings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent s = new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(s);
+            }
+        });
     }
+
+
+
 }
